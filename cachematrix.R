74b5b6f5c904e2aca41eb07a81ba4ad8c1be52ef
Jennings <jennbryc@amazon.com>
2019-01-03 18:01:29 -0500

    Modified original function from rdpeng to allow caching of a matrix object and its' corresponding inversion.

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,26 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix function uses lexical scoping to assign a matrix
+## from the global environment to a cached value within the function
+## environment. This can then be used in conjunction with cacheSolve
+## to store the inverse of a matrix object. 
+##
+## Cached value can be manipulated using functions defined in the 
+## resulting list (set,get,setinverse,getinverse)
+## 
+## New matrix object inversion must be instantiated with cacheSolve
+##
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+      x <<- y
+      m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) m <<- inverse
+    getinverse <- function() m
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
@@ -12,4 +28,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    m <- x$getinverse()
+    if(!is.null(m)) {
+      message("getting cached data")
+      return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinverse(m)
+    m
 }
